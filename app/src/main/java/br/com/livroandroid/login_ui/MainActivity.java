c1c04446018adfb60c4d3b375eb443a0526509eb
washingtondeveloper <washingtonprogramador@gmail.com>
2019-07-17 21:30:39 -0300

    first commit

---
+++ app/src/main/java/br/com/livroandroid/login_ui/MainActivity.java
@@ -0,0 +1,34 @@
+package br.com.livroandroid.login_ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout rellay1, rellay2;
+    private Handler handler = new Handler();
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay1.setVisibility(View.VISIBLE);
+            rellay2.setVisibility(View.VISIBLE);
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
+        rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
+
+        handler.postDelayed(runnable, 2000);
+
+    }
+}
